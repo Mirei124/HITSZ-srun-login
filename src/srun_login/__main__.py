91d657fac382a2b3815d63ae6a24d2a14ff1cbe6
Mirei124 <chkycc@gmail.com>
2023-09-12 23:55:35 +0800

    cli: add user_ip option

--- src/srun_login/__main__.py
+++ src/srun_login/__main__.py
@@ -5,15 +5,18 @@ import sys
 def login():
     args = sys.argv
     if len(args) < 2:
-        print('usage: srun_login username [password]')
+        print('usage: srun_login username [password] [ip]')
         exit(0)
-    elif len(args) == 2:
-        username = args[1]
+    username = args[1]
+    if len(args) == 2:
         password = input('password: ')
     else:
-        username = args[1]
         password = args[2]
-    sl = SrunLogin(username, password)
+    if len(args) > 3:
+        ip = args[3]
+    else:
+        ip = None
+    sl = SrunLogin(username, password, ip)
     result = sl.run()
     exit(sl.check_login_result(result))
 
